85f0b8b8bf7b9be0641c36d6c9d4959ead6a5a0f
William Lewis <william.lewis@wework.com>
2018-02-09 11:58:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,3 @@
 
 
 
-def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)
-    return 'FizzBuzz'
-  elsif (number % 3 == 0)
-    return 'Fizz'
-  elsif (number % 5 == 0)
-    return 'Buzz'
-  else
-    #puts 'no fizzbuzz today'
-    return nil
-end
